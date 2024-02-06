8ec83358136c3a64f9dfcd6d8c1f621a06524bc9
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-02-06 03:50:06 -0300

    Atualiza o arquivo autotools/c/gtk/1/run_example.sh

--- autotools/c/gtk/1/run_example.sh
+++ autotools/c/gtk/1/run_example.sh
@@ -29,12 +29,7 @@ source deactivate_conanbuild.sh
 # Make dynamic library available on PATH
 source conanrun.sh
 
-output=$(./string_formatter)
-
-if [[ "$output" != 'Conan - The C++ Package Manager!' ]]; then
-	echo "ERROR: The String Formatter output does not match with the expected value: 'Conan - The C++ Package Manager!'"
-	exit 1
-fi
+./hello
 
 echo 'AutotoolsToolchain example has been executed with SUCCESS!'
 exit 0
