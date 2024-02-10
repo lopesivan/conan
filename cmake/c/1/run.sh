851bfb73f2dc9677570e0a17947b4faf9bbf9cd3
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-02-10 03:52:23 -0300

    Atualiza o arquivo cmake/c/1/run.sh

--- cmake/c/1/run.sh
+++ cmake/c/1/run.sh
@@ -12,4 +12,7 @@ conan install . --output-folder=build --build=missing
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
+#run
 ./hello
+# informação sobre o compilador
+objdump -s --section .comment hello
