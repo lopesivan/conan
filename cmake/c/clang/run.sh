668146b29645f051c91025afe834e0f11955771b
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-02-10 04:02:05 -0300

    Renomeia o arquivo cmake/c/1/CMakeLists.txt ➜ cmake/c/clang/CMakeLists.txt

--- cmake/c/clang/run.sh
+++ cmake/c/clang/run.sh
@@ -10,7 +10,7 @@ rm -rf build
 
 conan install . --output-folder=build --build=missing
 cd build
-cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+CC=clang CXX=clang++ cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
 cmake --build .
 #run
 ./hello
