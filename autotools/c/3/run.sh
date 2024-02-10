c37ca1a9e2ce4ea62ee4356fe2a09bc0ca8ff671
Ivan Lopes <lopesivan.ufrj@gmail.com>
2024-02-10 11:22:42 -0300

    Adiciona o arquivo autotools/c/3/Makefile.am

---
+++ autotools/c/3/run.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+echo "- AutotoolsToolchain: The toolchain generator for Autotools -"
+
+set -ex
+
+# Remove cache
+# if exist path `build' then remove.
+_d=build
+test -d $_d && rm -rf $_d
+
+# Then generate conanbuild.sh
+conan install . --output-folder=build --build=missing
+
+cd build
+ln -s ../configure.ac
+ln -s ../Makefile.am
+ln -s ../src
+source conanbuild.sh
+
+# Build the example
+aclocal
+automake --add-missing
+autoconf
+./configure
+make
+
+source deactivate_conanbuild.sh
+
+# Make dynamic library available on PATH
+source conanrun.sh
+
+#run
+#./hello
+# informação sobre o compilador
+objdump -s --section .comment hello
+exit 0
